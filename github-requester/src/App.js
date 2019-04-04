c89cf5d2e4d85f24c4ab54cd910dcd2be9f0a8e6
Naguib Ihab <nick@upilio.com>
2019-04-04 13:14:49 +1100

    finished making http request in react

---
+++ github-requester/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      username: ''
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick () {
+    axios.get('https://api.github.com/users/naguibihab')
+    .then(response => {
+      console.log(response.data)
+      this.setState({username: response.data.name})
+    })
+  }
+
+  render () {
+    return (
+      <div className='button__container'>
+        <button className='button' onClick={this.handleClick}>
+          Click Me
+        </button>
+        <p>{this.state.username}</p>
+      </div>
+    )
+  }
+}
+export default App
